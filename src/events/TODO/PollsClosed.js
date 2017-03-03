3aba9d3a1cbc6694d8efaaf5355d0ab0edb36dea
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-03-02 18:15:48 -0800

    TODOs

---
+++ src/events/TODO/PollsClosed.js
@@ -0,0 +1,9 @@
+//TODO: Implement ClosePoll command 
+//TODO: REST API
+//TODO: PollResults read model
+
+export default class PollsClosed {
+  constructor(referendumId) {
+    this.referendumId = referendumId; 
+  }
+};
