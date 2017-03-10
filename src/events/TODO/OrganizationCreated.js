0ecfa041ea4b62a2ad1bc51a816f8587c24b968c
Robert Reppel <robertreppel@users.noreply.github.com>
2017-03-10 07:13:10 -0800

    Update OrganizationCreated.js

---
+++ src/events/TODO/OrganizationCreated.js
@@ -0,0 +1,4 @@
+// TODO: OrganizationCreated event, CreateOrganization command, "Organization" aggregate,  /organization/create REST endpoint (POST).
+// Would be nice if we could secure the API via http://passportjs.org as part of this: For now, organizations can only be created by 
+// a "superadmin": A config file contains his/her Google userId (The "sub" field from https://developers.google.com/identity/protocols/OpenIDConnect?hl=EN ).
+//
