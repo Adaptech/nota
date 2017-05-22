073c8e4e446bf8bd069c53b807acc294ef59a050
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-05-22 07:42:27 -0700

    More descriptive behaviour tests.

--- api/test/referendum_votecast_test.js
+++ api/test/referendum_votecast_test.js
@@ -84,41 +84,62 @@ describe('Holding Referendums: Casting Votes', function() {
         );
       })
     })
-
-    describe('When trying to cast a vote without being an authenticated voter', function () {
-      it('should not be possible', function () {
-        let referendum = new Referendum();
-        referendum.hydrate(new ReferendumCreated("134", "org-1", "Referendum on the United Klingon's membership of the European Union", "Should the United Klingon remain a member of the European Union?", options));
-        referendum.hydrate(new PollsOpened(referendumId));
-        assert.throws(
-          () => {
-            referendum.execute(new CastVote(referendumId, "voter-1", "Leave the European Union"));
-          },
-          function (err) {
-            if (err.name == "ValidationFailed" && err.message.find(m => m.field && m.msg === "Voter is not authenticated.")) {
-              return true;
-            }
-          },
-          'unexpected error'
-        );
-      })
-    })
   })
 
-  describe('Given a referendum and that polls are not open', function () {
+  describe('When polls are not open', function () {
     var referendum = new Referendum();
     var options = ["Remain a member of European Union", "Leave the European Union"];
     let referendumId = "134"
     let vote = "Remain a member of European Union";
 
     referendum.hydrate(new ReferendumCreated("134", "org-1", "Referendum on the United Klingon's membership of the European Union", "Should the United Klingon remain a member of the European Union?", options));
-    it('when casting a vote, voting should not be possible', function () {
+    it('voting should not be possible', function () {
       assert.throws(
         () => {
           referendum.execute(new CastVote(referendumId, "123", vote));
         },
         function (err) {
           if (err.name == "ValidationFailed" && err.message.find(m => m.msg === "Polls are not open.")) {
+            return true;
+          }
+        },
+        'unexpected error'
+      );
+    })
+  })
+
+  describe('When trying to cast a vote without being an authenticated voter', function () {
+    it('should not be possible', function () {
+      let referendum = new Referendum();
+      let options = ["Remain a member of European Union", "Leave the European Union"];
+      referendum.hydrate(new ReferendumCreated("134", "org-1", "Referendum on the United Klingon's membership of the European Union", "Should the United Klingon remain a member of the European Union?", options));
+      referendum.hydrate(new PollsOpened("referendum-1"));
+      assert.throws(
+        () => {
+          referendum.execute(new CastVote("referendum-1", "voter-1", "Leave the European Union"));
+        },
+        function (err) {
+          if (err.name == "ValidationFailed" && err.message.find(m => m.field && m.msg === "Voter is not authenticated.")) {
+            return true;
+          }
+        },
+        'unexpected error'
+      );
+    })
+  })
+
+  describe('When trying to cast a vote without being an authenticated voter', function () {
+    it('should not be possible', function () {
+      let referendum = new Referendum();
+      let options = ["Remain a member of European Union", "Leave the European Union"];
+      referendum.hydrate(new ReferendumCreated("134", "org-1", "Referendum on the United Klingon's membership of the European Union", "Should the United Klingon remain a member of the European Union?", options));
+      referendum.hydrate(new PollsOpened("referendum-1"));
+      assert.throws(
+        () => {
+          referendum.execute(new CastVote("referendum-1", "voter-1", "Leave the European Union"));
+        },
+        function (err) {
+          if (err.name == "ValidationFailed" && err.message.find(m => m.field && m.msg === "Voter is not authenticated.")) {
             return true;
           }
         },
