7eefeaa622fab14c3a303e174d58483fc9baca39
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-05-23 16:32:31 -0700

    Infrastructure upgrade - voter list read model

--- api/src/readModels/referendum.js
+++ api/src/readModels/referendum.js
@@ -6,13 +6,13 @@ export function handler(referendumList, eventData) {
   const event = eventData.event;
   switch(eventData.typeId) {
     case 'ReferendumCreated':
-        referendumList.create({
-          referendumId: event.referendumId,
-          organizationId: event.organizationId,
-          name: event.name,
-          proposal: event.proposal,
-          options: event.options, 
-        });
+      referendumList.create({
+        referendumId: event.referendumId,
+        organizationId: event.organizationId,
+        name: event.name,
+        proposal: event.proposal,
+        options: event.options, 
+      });
       break;
   }
   return referendumList;
--- api/src/readModels/voterlist.js
+++ api/src/readModels/voterlist.js
@@ -6,21 +6,15 @@ export function handler(voterList, eventData) {
   const event = eventData.event;
   switch(eventData.typeId) {
     case 'VoterRegistered':
-      var voterListEntryIndex = voterList.findIndex(isInListAlready, eventData.event.voterId);
-      if(voterListEntryIndex === -1) {
-        voterList.push({
-          voterId: event.voterId,
-          organizationId: event.organizationId,
-          firstname: event.firstname,
-          lastname: event.lastname,
-          address: event.address, 
-        });
-      }
+      voterList.create({
+        voterId: event.voterId,
+        organizationId: event.organizationId,
+        firstname: event.firstname,
+        lastname: event.lastname,
+        address: event.address, 
+      });
       break;
   }
   return voterList;
 }
 
-function isInListAlready(voter) {
-    return voter.voterId === this;
-}
