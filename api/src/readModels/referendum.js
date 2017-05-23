9b1eabbe563cd3312fef70679d6bc44742dded65
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-05-23 16:30:19 -0700

    Infrastructure upgrade - referendum list read model

--- api/src/readModels/referendum.js
+++ api/src/readModels/referendum.js
@@ -6,35 +6,14 @@ export function handler(referendumList, eventData) {
   const event = eventData.event;
   switch(eventData.typeId) {
     case 'ReferendumCreated':
-      var referendumRecordIndex = referendumList.findIndex(isInListAlready, eventData.event.referendumId);
-      if(referendumRecordIndex === -1) {
-        referendumList.push({
+        referendumList.create({
           referendumId: event.referendumId,
           organizationId: event.organizationId,
           name: event.name,
           proposal: event.proposal,
           options: event.options, 
         });
-      }
-      break;
-    case 'VoteCast':
-      var referendumRecordIndex = referendumList.findIndex(isInListAlready, eventData.event.referendumId);
-      if(referendumRecordIndex === -1) {
-        throw new Error("Referendum " + event.referendumId + ": Referendum with referendumId " + event.referendumId + " not found.");
-      }
-      let referendum = referendumList[referendumRecordIndex];
-      let currentTallyForOption = referendum.options[event.vote];
-      referendum.options[event.vote] = currentTallyForOption + 1;
-      referendumList = [
-        ...referendumList.slice(0, referendumRecordIndex),
-        referendum,
-        ...referendumList.slice(referendumRecordIndex+1)
-      ]
       break;
   }
   return referendumList;
-}
-
-function isInListAlready(referendum) {
-    return referendum.referendumId === this;
 }
