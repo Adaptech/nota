60a07ad638ee3c5cfa89c5bcfe260094db91d1b0
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-05-24 12:55:14 +0200

    voterlist readmodel tests upgraded to new infrastructure.

--- api/test/voterlist_readmodel_test.js
+++ api/test/voterlist_readmodel_test.js
@@ -13,13 +13,6 @@ describe('voterlist', function() {
     let lastname = "Lastname";
     let address = new PostalAddress("streetAddress", "postOfficeBoxNumber", "addressLocality", "WA", "94043", "addressCountry")
     let voterRegistered = new VoterRegistered(voterId, organizationId, firstname, lastname, address)
-    const eventData = {
-        typeId: 'VoterRegistered',
-        event: voterRegistered,
-        metadata: null
-    };
-
-
     let voterList = [];
     setUpReadModelTests({
       readModels: {voterList: voterListReadmodel},
@@ -31,29 +24,26 @@ describe('voterlist', function() {
       assert.ok(voterList.length === 1)
       assert.equal(voterList[0].voterId, voterId)
       assert.equal(voterList[0].organizationId, organizationId)
-      assert.equal(voterList[0].firstname, firstname)
-      assert.equal(voterList[0].lastname, lastname)
-      assert.equal(voterList[0].address, address)
     })
   })
-  // describe('Given that a voter is already in the voter list', function() {
-  //   let voterId = "voter-1";
-  //   let organizationId = "org-1";
-  //   let firstname = "Jim";
-  //   let lastname = "Lastname";
-  //   let address = new PostalAddress("streetAddress", "postOfficeBoxNumber", "addressLocality", "WA", "94043", "addressCountry")
-  //   let voterRegistered = new VoterRegistered(voterId, organizationId, firstname, lastname, address)
-  //   const eventData = {
-  //       typeId: 'VoterRegistered',
-  //       event: voterRegistered,
-  //       metadata: null
-  //   };
-  //   let voterList = [];
-  //   voterList = voterListReadmodel.handler(voterList, eventData);
-  //   it('the voter should be in the voter list only once when VoterRegistered is received a second time', function() {
-  //     voterList = voterListReadmodel.handler(voterList, eventData);
-  //     assert.ok(voterList.length === 1)
-  //     assert.equal(voterList[0].voterId, voterId)
-  //   })
-  // })
+  describe('Given that a voter is already in the voter list', function() {
+    let voterId = "voter-1";
+    let organizationId = "org-1";
+    let firstname = "Jim";
+    let lastname = "Lastname";
+    let address = new PostalAddress("streetAddress", "postOfficeBoxNumber", "addressLocality", "WA", "94043", "addressCountry")
+    let voterRegistered = new VoterRegistered(voterId, organizationId, firstname, lastname, address)
+    let voterList = [];
+    setUpReadModelTests({
+      readModels: {voterList: voterListReadmodel},
+      events: [voterRegistered ],
+      resultsSetter: result => voterList = result
+    })
+
+
+    it('the voter should be in the voter list only once when VoterRegistered is received a second time', function() {
+      assert.ok(voterList.length === 1)
+      assert.equal(voterList[0].voterId, voterId)
+    })
+  })
 })
